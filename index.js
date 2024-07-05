9bb3503957ccaaca31365d74852c8112e4e81e0b
Mohd Sohail <42858553+Mohdsohail7@users.noreply.github.com>
2024-07-05 12:17:53 +0000

    Original array fixed

--- index.js
+++ index.js
@@ -335,7 +335,7 @@ app.get('/products/filter/price', (req, res) => {
 
 // Endpoint 8: Send original array of products
 app.get('/products', (req, res) => {
-  res.json({originalArray: products});
+  res.json({products: products});
 });
 
 let port = 8000;
